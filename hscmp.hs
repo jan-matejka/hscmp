d67960fdf0fa10c4b8de076a30be6e612a459b49
Jan (yac) Matějka <JMatejka@suse.cz>
2014-06-06 20:36:48 +0200

    Implement -s argument

--- hscmp.hs
+++ hscmp.hs
@@ -9,15 +9,44 @@ import System.IO.Error
 
 usage prog = "Usage: " ++ prog ++ " [-s] file1 file2"
 
+data Args = Silent | File String
+
+instance Eq Args where
+    Silent == Silent = True
+    File x == File y = x == y
+    _ == _ = False
+
+parseArgs [] = []
+parseArgs (a:args)
+    | any ((==) a) ["-s", "--silent", "--quiet"] = parseArgs args ++ [Silent]
+    | head a == '-' = error $ "Unknown argument " ++ a
+    | otherwise = parseArgs args ++ [File a]
+
+files [] = []
+files (a:args) = case a of
+    File x -> files args ++ [x]
+    otherwise -> files args
+
+has_silent pargs = any ((==) Silent) pargs
+
+printResult Same _  _  = exitWith ExitSuccess
+printResult d True  _  = exitWith $ ExitFailure 1
+printResult d False fs = do
+    hPutStrLn stderr $ join " " $ fs ++ [show d]
+    printResult d True fs
+
 main = do
     args <- getArgs
-    if length args /= 2
+    let pargs = parseArgs args
+    let files' = files pargs
+
+    if length files' /= 2
         then do
             prog <- getProgName
             putStrLn $ usage prog
         else do
-            let x = head args
-                y = last args
+            let x = head files'
+                y = last files'
             result <- tryIOError $ compareF x y
             case result of
                 Left err -> do
@@ -25,5 +54,4 @@ main = do
                     exitWith $ ExitFailure 2
                 Right d -> case d of
                     Same -> exitWith ExitSuccess
-                    Differ _ _ -> do
-                        hPutStrLn stderr $ x ++ " " ++ y ++ " " ++ show d
+                    Differ _ _ -> printResult d (has_silent pargs) [x,y]
